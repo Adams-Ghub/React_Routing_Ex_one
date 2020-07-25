3f387f65c2c8ff4e3f11cf56c990fd3eace918a1
Adams <bamfoadamsf@gmail.com>
2020-07-25 12:50:03 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router,Route,Link} from "react-router-dom";
+
+import './customcss/style.css';
+
+
 
 function App() {
+  function HomeComponent(){
+    return(
+      <div>
+        <h1>Homepage</h1>
+      </div>
+    );
+
+  };
+  function AboutComponent(){
+    return(
+      <div>
+        <h1>About us</h1>
+      </div>
+    );
+
+  };
+  function ContactComponent(){
+    return(
+      <div>
+        <h1>Contact us</h1>
+      </div>
+    );
+
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Router>
+        <div >
+          <Link to="/home" className="links">Home</Link>
+          <Link to="/about" className="links">About</Link>
+          <Link to="/contact" className="links">Contact</Link>
+          <Route exact={true} path="/home" component={HomeComponent}/>
+          <Route exact={true} path="/about" component={AboutComponent}/>
+          <Route exact={true} path="/contact" component={ContactComponent}/>
+        </div>
+      </Router>
+    
+      
     </div>
   );
 }
